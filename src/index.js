754360d9af2f286ed32248bbc7d363dd15f7ff30
Joel Joys <joeljoys.official@gmail.com>
2020-09-21 00:11:22 +0530

    redirection if already signed in v5.1

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, withRouter } from "react-router-dom";
 import Register from "./Auth/Register";
 import Login from "./Auth/Login";
 import "semantic-ui-css/semantic.min.css";
+import firebase from "firebase";
 
-const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={App} />
-      <Route path="/register" exact component={Register} />
-      <Route path="/login" exact component={Login} />
-    </Switch>
-  </BrowserRouter>
-);
+class Routes extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.props.history.push("/");
+      }
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/login" exact component={Login} />
+      </Switch>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(Routes);
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <BrowserRouter>
+      <RootWithAuth />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
